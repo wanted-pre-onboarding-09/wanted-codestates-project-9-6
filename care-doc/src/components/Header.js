723f8b14f1606cdee040186da2d81907f276d273
y-solb <ysolb99@gmail.com>
2022-02-23 03:36:49 +0900

    feat: 이전 버튼 클릭 시 돌아갈 수 있도록 Link 추가

--- care-doc/src/components/Header.js
+++ care-doc/src/components/Header.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { ReactComponent as PrevBtn } from '../assets/Fill.svg';
 import styles from '../css/Header.module.css';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Header = ({ displayNone }) => {
   const displayType = displayNone ? styles.none : '';
   return (
     <header className={styles.header}>
-      <PrevBtn className={`${styles.prev__btn} ${displayType}`} />
+      <Link to="/">
+        <PrevBtn className={`${styles.prev__btn} ${displayType}`} />
+      </Link>
       <span className={styles.title}>돌보미 신청하기</span>
     </header>
   );

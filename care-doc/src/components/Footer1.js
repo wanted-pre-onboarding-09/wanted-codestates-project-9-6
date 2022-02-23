b1b6c87bb22d322850d402ee5b5833548c43cd76
y-solb <ysolb99@gmail.com>
2022-02-23 20:51:14 +0900

    Feat: 페이지 이동 추가 & 신청서 제출

--- care-doc/src/components/Footer1.js
+++ care-doc/src/components/Footer1.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from '../css/Footer1.module.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { nextStep, previousStep } from '../store/pageStep';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Footer1 = () => {
   const { currentStep } = useSelector(({ pageStep }) => pageStep);
@@ -15,6 +17,37 @@ const Footer1 = () => {
     dispatch(previousStep(currentStep));
   };
 
+  const navigate = useNavigate();
+
+  const moveToLast = () => {
+    const body = {
+      address: {
+        locationCode: '11',
+        roadCode: '11',
+        roadAddress: '11',
+        jibunAddress: '11',
+        sidoName: '11',
+        sigunguName: '11',
+        myundongName: '11',
+        liName: 's11',
+        addressDetail: '11',
+      },
+      phoneNumber: '11',
+      schedule: {
+        startDate: '11',
+        endDate: '11',
+        visitTime: '11',
+        hour: 0,
+      },
+      workType: 'TIME',
+    };
+
+    axios
+      .post('https://caredoc-fe-server.herokuapp.com/application', body)
+      .then((res) => console.log(res));
+    navigate('/care/final');
+  };
+
   const btnValidation = (pageNumber) => {
     switch (pageNumber) {
       case 1:
@@ -22,9 +55,9 @@ const Footer1 = () => {
       case 2:
         return false;
       case 3:
-        return true;
+        return false;
       case 4:
-        return true;
+        return false;
       default:
         return null;
     }
@@ -33,7 +66,11 @@ const Footer1 = () => {
   return (
     <div className={styles.footerWrapper}>
       <button onClick={prevClick}>이전</button>
-      <button onClick={nextClick} disabled={btnValidation(currentStep)}>
+
+      <button
+        onClick={currentStep === 4 ? moveToLast : nextClick}
+        disabled={btnValidation(currentStep)}
+      >
         다음
       </button>
     </div>
